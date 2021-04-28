1c8e8029efa74799fe96b90adb328bc6dca16721
Jad Mashtoub <jadmashtoub1@gmail.com>
2021-04-28 21:15:23 +1000

    first

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NestedRep2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the width you desire");
+            int width = int.Parse( Console.ReadLine() );
+            Console.Write("x");
+            for(int i = 1; i <= width;  i++  )
+            {
+                for (int x = 1; x <= i; x++)
+                {
+                    Console.Write("x");
+                }
+                Console.WriteLine();
+            }
+        } 
+    }
+}
